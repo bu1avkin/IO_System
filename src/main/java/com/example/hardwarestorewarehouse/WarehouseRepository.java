fe8ad0de5a96922133679ee231c79ab4c80994e2
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-02-27 02:30:33 +0300

    Created a Quary.

--- src/main/java/com/example/hardwarestorewarehouse/WarehouseRepository.java
+++ src/main/java/com/example/hardwarestorewarehouse/WarehouseRepository.java
@@ -1,4 +1,13 @@
 package com.example.hardwarestorewarehouse;
 
-public interface WarehouseRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.relational.core.sql.Select;
+
+
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long>{
+    @Query("SELECT p FROM Warehouse p WHERE CONCAT(p.equipment_type, ''. p.equipment_group, '', p.date_imported, '', p.date_exported, '', p.driver_name) LIKE %?1%")
+    List<Warehouse> search(String keywoard);
 }
