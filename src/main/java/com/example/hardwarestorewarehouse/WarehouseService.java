a116315c70badaf1d922a2d89ecc8ebbd0f896c5
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-02-27 02:40:38 +0300

    Wrote Class for search by keyword.

--- src/main/java/com/example/hardwarestorewarehouse/WarehouseService.java
+++ src/main/java/com/example/hardwarestorewarehouse/WarehouseService.java
@@ -1,4 +1,31 @@
 package com.example.hardwarestorewarehouse;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WarehouseService {
+    @Autowired
+    private WarehouseRepository repo;
+
+    public List<Warehouse> listAll(String keyword){
+        if (keyword != null){
+            return repo.search(keyword);
+        }
+        return repo.findAll();
+    }
+
+    public void save(Warehouse warehouse) {
+        repo.save(warehouse);
+    }
+
+    public Warehouse get(Long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
 }
