8a27eb816b7ff236672494021ca4722617b74417
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-02-27 02:48:47 +0300

    This will start up my application.

--- src/main/java/com/example/hardwarestorewarehouse/WarehouseManagerApplication.java
+++ src/main/java/com/example/hardwarestorewarehouse/WarehouseManagerApplication.java
@@ -1,4 +1,13 @@
 package com.example.hardwarestorewarehouse;
 
-public class WarehouseManagerApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WarehouseManagerApplication extends SpringBootServletInitializer{
+
+    public static void main(String[] args){
+        SpringApplication.run(WarehouseManagerApplication.class, args);
+    }
 }
