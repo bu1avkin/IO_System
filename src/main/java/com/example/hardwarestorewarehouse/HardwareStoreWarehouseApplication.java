fc939a6d7908366a631c5737b9fa995fd77dfdc5
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-02-27 01:35:27 +0300

    Initialized project.

---
+++ src/main/java/com/example/hardwarestorewarehouse/HardwareStoreWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hardwarestorewarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareStoreWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareStoreWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hardwarestorewarehouse/HardwareStoreWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hardwarestorewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareStoreWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
