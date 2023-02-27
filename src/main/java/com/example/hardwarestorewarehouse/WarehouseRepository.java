78e9565c7456314c2b3666a79c63944e8b74d0ce
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-02-27 03:15:54 +0300

    Created a Query for my application

--- src/main/java/com/example/hardwarestorewarehouse/WarehouseRepository.java
+++ src/main/java/com/example/hardwarestorewarehouse/WarehouseRepository.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.relational.core.sql.Select;
 
 
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long>{
-    @Query("SELECT p FROM Warehouse p WHERE CONCAT(p.equipment_type, ''. p.equipment_group, '', p.date_imported, '', p.date_exported, '', p.driver_name) LIKE %?1%")
-    List<Warehouse> search(String keywoard);
+public interface WarehouseRepository extends JpaRepository<Machine, Long>{
+    @Query("SELECT p FROM Warehouse p WHERE CONCAT(p.equipment_type, '', p.equipment_group, '', p.date_imported, '', p.date_exported, '', p.driver_name) LIKE %?1%")
+    List<Machine> search(String keywoard);
 }
